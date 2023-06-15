cea028f10bfa7960bfabd456cf976f15c4fbfdf1
David Park <parkman0718@naver.com>
2023-06-15 23:32:14 +0900

    Day1 Commit
    * 플레이어 이동
    * 간단 애니메이션 구현
    * 맵추가, 카메라 맵 멋어나지 않게 반경 제한

---
+++ Assets/02.Script/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Vector2 inputVec;
+    [SerializeField] float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer sprite;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void LateUpdate()
+    {
+        //백터의 크기
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        //키 눌리면 1 아니면 0
+        if (inputVec.x != 0){            
+            sprite.flipX = inputVec.x < 0; //left : -1, right : +1
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+
+
+}
